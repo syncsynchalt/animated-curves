ae482089570537267b7660929f88ec55d6bffd26
Michael Driscoll <michael@ulfheim.net>
2022-06-13 21:32:28 -0600

    Bookmark links

--- site/fixups.js
+++ site/fixups.js
@@ -16,4 +16,24 @@
             numTag.textContent = numTag.textContent.slice(0, toKeep) + '…' + numTag.textContent.slice(-toKeep);
         }
     }
+
+    // make all headers bookmark-able
+    let linksUsed = {};
+    for (let i of [1, 2, 3, 4, 5, 6]) {
+        let hTags = document.getElementsByTagName(`h${i}`);
+        for (let tag of hTags) {
+            const wrap = document.createElement('a');
+            wrap.classList.add('no-vis-link');
+            let link = tag.textContent.toLowerCase()
+                .replaceAll(/[^a-z\d]/g, '-')
+                .replaceAll(/-+/g, '-')
+                .replaceAll(/^-|-$/g, '');
+            while (linksUsed[link]) link += '-dup';
+            linksUsed[link] = 1;
+            wrap.href = `#${link}`;
+            tag.id = link;
+            tag.parentNode.replaceChild(wrap, tag);
+            wrap.appendChild(tag);
+        }
+    }
 })();
