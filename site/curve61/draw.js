977138079235876573a0b6504a5b4fca0a1c3c5c
Michael Driscoll <michael@ulfheim.net>
2022-06-14 14:49:21 -0600

    Look good on mobile

--- site/curve61/draw.js
+++ site/curve61/draw.js
@@ -741,9 +741,14 @@ async function runAddPDemo(ctx, nQ, Q, updateCb, drawDoneCb) {
 
 function pickGoodDoubleAddNumber() {
     let result = 0;
-    const picks = [0, 0, 1, 1, 1];
-    // noinspection JSCheckFunctionSignatures
-    picks.sort(() => { return Math.random() > 0.5 });
+    const picks = [0, 0, 0, 1, 1, 1];
+    let shuffleArray = (arr) => {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    };
+    shuffleArray(picks);
     for (let i = 0; i <= 5; i++) {
         if (picks[i]) {
             result += 2**i;
